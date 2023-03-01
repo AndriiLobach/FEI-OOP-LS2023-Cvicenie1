a9cb30ca565753817eebefc5fbbc472566ebf6d4
PC1 <xlobach@stuba.sk>
2023-03-02 00:34:09 +0100

    initial commit

---
+++ FEI-OOP-LS2023-Cvicenie1/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module sk.stuba.fei.oop.feioopls2023cvicenie1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens sk.stuba.fei.oop.feioopls2023cvicenie1 to javafx.fxml;
+    exports sk.stuba.fei.oop.feioopls2023cvicenie1;
+}
---
+++ FEI-OOP-LS2023-Cvicenie1/src/main/java/sk/stuba/fei/oop/feioopls2023cvicenie1/App.java
@@ -0,0 +1,8 @@
+package sk.stuba.fei.oop.feioopls2023cvicenie1;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
