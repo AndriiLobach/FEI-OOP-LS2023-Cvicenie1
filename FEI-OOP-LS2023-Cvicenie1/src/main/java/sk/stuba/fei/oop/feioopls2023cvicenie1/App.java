1c5d1d6fd211ef8269cadc827fd0b58e00e6bce2
PC1 <xlobach@stuba.sk>
2023-03-02 00:45:22 +0100

    zvysok

--- FEI-OOP-LS2023-Cvicenie1/src/main/java/sk/stuba/fei/oop/feioopls2023cvicenie1/App.java
+++ FEI-OOP-LS2023-Cvicenie1/src/main/java/sk/stuba/fei/oop/feioopls2023cvicenie1/App.java
@@ -3,6 +3,8 @@ package sk.stuba.fei.oop.feioopls2023cvicenie1;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int zvysok = 2%3;
+        System.out.println(zvysok);
     }
 }
 
