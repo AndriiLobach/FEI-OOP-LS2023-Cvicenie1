d9c2f479034d21712c9047d404ab7aa9e7235f39
PC1 <xlobach@stuba.sk>
2023-03-02 22:56:37 +0100

    Arrays

--- FEI-OOP-LS2023-Cvicenie1/src/main/java/sk/stuba/fei/oop/feioopls2023cvicenie1/Cvicenie2.java
+++ FEI-OOP-LS2023-Cvicenie1/src/main/java/sk/stuba/fei/oop/feioopls2023cvicenie1/Cvicenie2.java
@@ -1,21 +1,17 @@
 package sk.stuba.fei.oop.feioopls2023cvicenie1;
 
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Cvicenie2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = "a";
-        String b = "a";
-        System.out.println("zadajte text");
-        String c = sc.nextLine();
+        int[] x = new int[]{1, 2, 3};
+        int[] y = new int[3];
+        for (int i = 0; i < y.length; i++) {
+            y[i] = i + 1;
+        }
 
-        System.out.println("a == a " + (a == a));
-        System.out.println("a == b " + (a == b));
-        System.out.println("a == c " + (a == c));
-        System.out.println("Objects.equals(a, c) " + Objects.equals(a, c));
-        System.out.println("Objects.equals(b, c) " + Objects.equals(b, c));
-        System.out.println("a.equals(c) " + a.equals(c));
+        System.out.println(Arrays.equals(x, y));
     }
 }
