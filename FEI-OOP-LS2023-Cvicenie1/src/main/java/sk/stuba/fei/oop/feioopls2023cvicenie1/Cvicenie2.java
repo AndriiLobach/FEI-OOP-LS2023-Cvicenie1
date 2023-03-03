d6b22962f1be847a2fad0f6fbfdb87019243d047
PC1 <xlobach@stuba.sk>
2023-03-04 00:24:04 +0100

    spread operator

--- FEI-OOP-LS2023-Cvicenie1/src/main/java/sk/stuba/fei/oop/feioopls2023cvicenie1/Cvicenie2.java
+++ FEI-OOP-LS2023-Cvicenie1/src/main/java/sk/stuba/fei/oop/feioopls2023cvicenie1/Cvicenie2.java
@@ -6,17 +6,11 @@ import java.util.Scanner;
 
 public class Cvicenie2 {
     public static void main(String[] args) {
-        int[][] pole = new int[10][];
+        vypis("text", 5, 34, 6, 7);
+    }
 
-        for (int i = 0; i < pole.length; i++) {
-            pole[i] = new int[i + 1];
-            for (int j = 0; j < pole[i].length; j++) {
-                pole[i][j] = j;
-            }
-        }
-
-        for (int i = 0; i < pole.length; i++) {
-            System.out.println(Arrays.toString(pole[i]));
-        }
+    public static void vypis(String text, int... pole) {
+        System.out.println(text);
+        System.out.println(Arrays.toString(pole));
     }
 }
